ce31c4e15325263520c5441c62166fc0e1fdd519
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-26 23:15:16 +0500

    Created Function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 const input = document.getElementById("todo-input")
 const addBtn = document.getElementById("add-btn")
 const list = document.getElementById("todo-list")
+
+function saveTodos(){
+    
+}
