4f3e109f8881cb6f38db3770be67d4d0d8c0a06c
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-26 23:20:00 +0500

    Saved Tasks from Local Storage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const input = document.getElementById("todo-input")
 const addBtn = document.getElementById("add-btn")
 const list = document.getElementById("todo-list")
 
+const saved = localStorage.getItem("todos");
+const todos = saved? JSON.parse(saved) : [];
+
 function saveTodos(){
-    
+
 }
